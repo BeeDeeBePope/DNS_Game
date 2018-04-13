142ed723ef49049f50fa7c60f2ee0c9eb4b71ac7
204019 Trzeszczak Adam <204019@edu.p.lodz.pl>
2018-04-13 13:14:09 +0200

    Adding EnemyMovement logic and navmesh data

---
+++ Dr.Stealth/Assets/Scripts/EnemyMovemnt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.AI;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovemnt : MonoBehaviour
+{
+    public Transform[] PatrolPoints;
+
+    private NavMeshAgent navMeshAgent;
+    private int iterator;
+    private Transform currenWaypoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.isStopped = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (navMeshAgent.isStopped)
+        {
+            if (iterator >= PatrolPoints.Length)
+            {
+                iterator = 0;
+            }
+
+            currenWaypoint = PatrolPoints[iterator++];
+            navMeshAgent.SetDestination(currenWaypoint.position);
+            navMeshAgent.isStopped = false;
+        }
+
+        if (Math.Abs(transform.position.x - currenWaypoint.position.x) < .0001f && Math.Abs(transform.position.z - currenWaypoint.position.z) < .0001f)
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+    }
+}
