7852536f4cab8dacbb48a87b920835e01d74b8c7
Unknown <bysiek1998@gmail.com>
2018-04-13 12:43:27 +0200

    Adding movement and sneaking

--- Dr.Stealth/Assets/Scripts/PlayerMovement.cs
+++ Dr.Stealth/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float Speed = 5;
+    public float SneakingDivider = 2;
+    public float YMin = 0, YMax = 0.25f;
+    private Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Sneaking
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Vector3 movement = new Vector3(moveHorizontal, -1, moveVertical);
+            rb.velocity = movement * Speed / SneakingDivider;
+        }
+        else
+        {
+            Vector3 movement = new Vector3(moveHorizontal, 1, moveVertical);
+            rb.velocity = movement * Speed;
+        }
+        rb.position = new Vector3
+       (
+           this.transform.position.x,
+           Mathf.Clamp(rb.position.y, YMin, YMax),
+           this.transform.position.z
+       );
+
+
+    }
 }
