3a396c81b976abc3ad0b0f4311788f618883d355
Unknown <bysiek1998@gmail.com>
2018-04-15 13:16:29 +0200

    Adding player interaction and basic enemy interaction

---
+++ Dr.Stealth/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private IInteractable currentInterraction;
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (!collision.gameObject.CompareTag("Interactable"))
+        {
+            return;
+        }
+        currentInterraction = collision.gameObject.GetComponent<IInteractable>();
+    }
+    
+    private void OnTriggerStay(Collider collision)
+    {
+        if (currentInterraction == null)
+        {
+            return;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            currentInterraction.Interact();
+            HealingInteractable healingInteractable = currentInterraction as HealingInteractable;
+            if (healingInteractable!=null)
+            {
+                healingInteractable.isHealing = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (!collision.gameObject.CompareTag("Interactable"))
+        {
+            return;
+        }
+        if(currentInterraction == collision.gameObject.GetComponent<IInteractable>())
+        {
+            currentInterraction = null;
+        }
+    }
+
+}
