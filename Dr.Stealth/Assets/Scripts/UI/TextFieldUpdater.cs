6a905ce98adabba504417739ce22bde10aab5f14
Unknown <andrzej.mi@op.pl>
2018-04-15 14:50:41 +0200

    Adding Game controller and timer

---
+++ Dr.Stealth/Assets/Scripts/UI/TextFieldUpdater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Variables._Definitions;
+
+public class TextFieldUpdater : MonoBehaviour
+{
+    private Text text;
+    public StringVariable stringVariable;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = stringVariable.Value;
+    }
+}
